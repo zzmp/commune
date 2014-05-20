bb7bec80e65019a00fc0920277c5a79305ae6f69
zzmp <zmp@umich.edu>
2014-05-20 06:50:56 -0700

    Drop database on deployment.

--- server/db/config.js
+++ server/db/config.js
@@ -2,6 +2,9 @@ var mongoose = module.exports.mongoose = require('mongoose');
 
 mongoose.connect(process.env.DB_URL || 'mongodb://localhost/commune');
 
+// Drop existing database to prevent hung errors
+mongoose.connection.db.dropDatabase();
+
 module.exports.roomSchema = mongoose.Schema({
   leader: String,
   room: {type: String, unique: true},
